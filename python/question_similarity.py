a5ad8e745a5570738340b66b59324291c6e036c6
parvoberoi <parv_oberoi@yahoo.co.in>
2018-07-29 16:51:20 -0400

    Update question_similarity.py

--- python/question_similarity.py
+++ python/question_similarity.py
@@ -10,15 +10,15 @@ def predict_similarity(question_left, question_right):
 	cwd = os.getcwd()
 
 	# load final model
-	model = model_from_json(open(os.path.join(cwd,  "models/final_model.model")).read())
-	model.load_weights(os.path.join(cwd, "models/final_model.model") + '.weights')
+	model = model_from_json(open(os.path.join(cwd,  "models/lowercase_model_80_42")).read())
+	model.load_weights(os.path.join(cwd, "models/lowercase_model_80_42") + '.weights')
 
 	# load stored corpus TFIDF model for translating inputs
 	vectorizer = pickle.load(open(os.path.join(cwd, "models/final_tfidf.pickle"), "rb"))
 	word2tfidf = dict(zip(vectorizer.get_feature_names(), vectorizer.idf_))
 	
-	question_1 = unicode(question_left)
-	question_2 = unicode(question_right)
+	question_1 = unicode(question_left.lower())
+	question_2 = unicode(question_right.lower())
 
 	question_1_vector = get_text_vector(question_1, word2tfidf)
 	question_2_vector = get_text_vector(question_2, word2tfidf)
